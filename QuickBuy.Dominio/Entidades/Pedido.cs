bf668b09141a76cb9359ed555bf5693c05bfb847
ricardo.sofiati <ricardo.sofiati@gmail.com>
2020-05-26 11:22:55 -0300

    Criação das Entidades

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,27 @@
+﻿using QuickBuy.Dominio.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int IdUsuario { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public int NumeroEndereco { get; set; }
+        public int FormaPagamentoID { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
+
+        /// <summary>
+        /// O pedido deve ter pelo menos 1 Itens do Pedido 
+        /// ou muitos Itens do Pedido
+        /// </summary>
+        public ICollection<ItemPedido> ItensPedido { get; set; }
+    }
+}
